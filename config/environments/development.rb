cf0d3d4e9609748f6a4ddd5275c94323b70236c6
lnsjhnssn <144560735+lnsjhnssn@users.noreply.github.com>
2024-06-27 18:59:34 +0200

    add image to S3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,9 @@ Rails.application.configure do
   # Using the local settings for Active Storage
 config.active_storage.service = :local
 
+# Using the Amazon S3 settings for Active Storage
+config.active_storage.service = :amazon
+
   # Do not eager load code on boot.
   config.eager_load = false
 
